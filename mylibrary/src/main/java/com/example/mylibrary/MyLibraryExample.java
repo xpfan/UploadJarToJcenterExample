23fe51da58565ec28502062bdad9e5e2f2340bec
fanxianping <462736001@qq.com>
2016-11-01 09:58:13 +0800

    加入grpc，将proto编译后的java文件一起打包。

---
+++ mylibrary/src/main/java/com/example/mylibrary/MyLibraryExample.java
@@ -0,0 +1,10 @@
+package com.example.mylibrary;
+
+/**
+ * Created by xpfan on 2016/11/1.
+ */
+public class MyLibraryExample {
+    public String sayHello(String name){
+        return "Hello,"+name;
+    }
+}
